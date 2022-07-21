4613f8acd4d9ccdfbd912d0d4f34a49312900073
Nando Cova <nandocova@gmail.com>
2022-07-21 11:06:06 -0500

    Implement full game logic

--- game-script.js
+++ game-script.js
@@ -1,9 +1,19 @@
-const btnR = document.querySelector('#rock');
-const btnP = document.querySelector('#paper');
-const btnS = document.querySelector('#scissors');
-const divR = document.querySelector('#results');
-const rslt = document.createElement('p');
-const mssg = document.createElement('p');
+const btnR        = document.querySelector('#rock');
+const btnP        = document.querySelector('#paper');
+const btnS        = document.querySelector('#scissors');
+const divR        = document.querySelector('#results');
+const buttons     = document.querySelector('#buttons');
+const body        = document.querySelector('body');
+const round       = document.createElement('p');
+const outcome     = document.createElement('p');
+const score       = document.createElement('p');
+const match       = document.createElement('p');
+const rules       = document.createElement('p');
+const results     = document.createElement('h3');
+const totalRounds = 5;
+var   wins        = 0;
+var   rounds      = 0;
+
 
 function getComputerChoice() {
     // Return randomly "Rock", "Paper" or "Scissors"
@@ -19,73 +29,83 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    divR.appendChild(rslt);
-    divR.appendChild(mssg);
+    // Match your choice agains the computer's
+    divR.appendChild(round);
+    divR.appendChild(outcome);
 
     if (playerSelection === "rock") {
         if (computerSelection === "rock") {
-            rslt.textContent = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
-            mssg.textContent = "It's a draw";
+            round.textContent   = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
+            outcome.textContent = "It's a draw!";
             return 0;
         } else if (computerSelection === "paper") {
-            rslt.textContent = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
-            mssg.textContent = "You lose! Paper beats Rock!";
+            round.textContent   = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
+            outcome.textContent = "Rock loses to Paper!";
             return -1;
         } else {
-            rslt.textContent = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
-            mssg.textContent = "You win! Rock beats Scissors!";
+            round.textContent   = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
+            outcome.textContent = "Rock beats Scissors!";
             return 1;
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            rslt.textContent = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
-            mssg.textContent = "You win! Paper beats Rock!";
+            round.textContent   = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
+            outcome.textContent = "Paper beats Rock!";
             return 1;
         } else if (computerSelection === "paper") {
-            rslt.textContent = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
-            mssg.textContent = "It's a draw!";
+            round.textContent   = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
+            outcome.textContent = "It's a draw!";
             return 0;
         } else {
-            rslt.textContent = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
-            mssg.textContent = "You lose! Scissors beat Paper!";
+            round.textContent   = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
+            outcome.textContent = "Paper loses to Scissors!";
             return -1;
         }
     } else {
         if (computerSelection === "rock") {
-            rslt.textContent = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
-            mssg.textContent = "You lose! Rock beats Scissors!";
+            round.textContent   = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
+            outcome.textContent = "Scissors lose to Rock!";
             return -1;
         } else if (computerSelection === "paper") {
-            rslt.textContent = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
-            mssg.textContent = "You win! Scissors beat Paper!";
+            round.textContent   = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
+            outcome.textContent = "Scissors beat Paper!";
             return 1;
         } else {
-            rslt.textContent = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
-            mssg.textContent = "It's a draw!";
+            round.textContent   = `${playerSelection.toUpperCase()} vs ${computerSelection.toUpperCase()}`
+            outcome.textContent = "It's a draw!";
             return 0;
         }
     }
 }
 
-function game() {
-    let score = 0;
-    let totalGames = 5;
+function play(choice) {
+    // Play a number of rounds and keep player's score
+    if (rounds < totalRounds) {
+        wins += playRound(choice, getComputerChoice());
+        score.textContent = `Score: ${wins}`;
+        divR.appendChild(score);
 
-    for (let i = 0; i < totalGames; i++) {
-        // Play five rounds
-        score = score + playRound(prompt("Choose: ").trim().toLowerCase(), getComputerChoice());
-        console.log(`round ${i+1}: ${score}`)
+        rounds++;
     }
 
-    if (score > 0) {
-        return "You win the game!";
-    } else if (score === 0) {
-        return "The game ended in a draw!";
-    } else {
-        return "You lose the game!";
+    match.textContent = "Rounds left: " + (totalRounds - rounds).toString();
+    divR.appendChild(match);
+    
+    if (rounds === totalRounds) {
+        if (wins > 0) {
+            results.textContent = "YOU WIN!";
+        } else if (wins < 0) {
+            results.textContent = "YOU LOSE!";
+        } else {
+            results.textContent = "IT WAS A DRAW";
+        }
+        divR.appendChild(results);
     }
 }
 
-btnR.addEventListener('click', () => playRound("rock", getComputerChoice()));
-btnP.addEventListener('click', () => playRound("paper", getComputerChoice()));
-btnS.addEventListener('click', () => playRound("scissors", getComputerChoice()));
+rules.textContent = "Play " + totalRounds + " rounds against the machine...";
+body.insertBefore(rules, buttons);
+
+btnR.addEventListener('click', () => play("rock"));
+btnP.addEventListener('click', () => play("paper"));
+btnS.addEventListener('click', () => play("scissors"));
